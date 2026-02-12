b696589e6b01c3c9973668d2363d21d881eb2ed5
Ezekiel <geniuswealthb@gmail.com>
2026-02-12 07:52:29 +0100

    Enfroce min withd

--- src/app/love/page.tsx
+++ src/app/love/page.tsx
@@ -122,20 +122,31 @@ export default function LovePage() {
                     <h1 className="text-3xl font-bold">My Cherry Blossom ❤️</h1>
 
                     <div>
-                        <div className="relative w-full md:w-85 max-w-95">
+                        <div className="relative w-full max-w-[380px] mx-auto">
                             <input
                                 type="date"
-                                className="px-4 py-2 pr-10 rounded-md text-white border border-gray-400 bg-black/10 w-full max-w-95 accent-white"
+                                className="
+      w-full
+      px-4 py-2 pr-10
+      rounded-md
+      text-white
+      border border-gray-400
+      bg-black/10
+      box-border
+      appearance-none
+    "
                                 value={input}
                                 onChange={(e) => {
                                     setErrorMessage("");
                                     setInput(e.target.value);
                                 }}
                             />
+
                             <span className="absolute right-3 top-1/2 -translate-y-1/2 text-white pointer-events-none">
                                 📅
                             </span>
                         </div>
+
 
 
                         {errorMessage && (
